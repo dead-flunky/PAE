a673f01800c2afec17b8d8e421600e332e06105d
Flunky <vale_mees@web.de>
2017-07-12 20:32:17 +0200

    doppelter codeblock entfernt

--- Assets/Python/Screens/CvMainInterface.py
+++ Assets/Python/Screens/CvMainInterface.py
@@ -2642,17 +2642,10 @@ class CvMainInterface:
                                         iBuilding = L.DPromosForPromoBuilding[iPromo]
                                         if not pCity.isHasBuilding(iBuilding):
                                             kBuildingInfo = gc.getBuildingInfo(iBuilding)
-                                            if not kBuildingInfo.isWater() or pCity.isCoastal(kBuildingInfo.getMinAreaSize()): #gc.getMIN_WATER_SIZE_FOR_OCEAN()
+                                            if not kBuildingInfo.isWater() or pCity.isCoastal(gc.getMIN_WATER_SIZE_FOR_OCEAN()): #kBuildingInfo.getMinAreaSize()
                                                 screen.appendMultiListButton("BottomButtonContainer", gc.getBuildingInfo(iBuilding).getButton(), 0, WidgetTypes.WIDGET_GENERAL, 719, iBuilding, False)
                                                 screen.show("BottomButtonContainer")
                                                 iCount = iCount + 1
-                                if pUnit.isHasPromotion(gc.getInfoTypeForString("PROMOTION_NAVIGATION4")) and pCity.isCoastal(gc.getMIN_WATER_SIZE_FOR_OCEAN()):
-                                    iBuilding = gc.getInfoTypeForString("BUILDING_PROMO_NAVI")
-                                    if not pCity.isHasBuilding(iBuilding):
-                                        screen.appendMultiListButton("BottomButtonContainer", gc.getBuildingInfo(iBuilding).getButton(), 0, WidgetTypes.WIDGET_GENERAL, 719, iBuilding, False)
-                                        screen.show("BottomButtonContainer")
-                                        iCount = iCount + 1
-                                # Ende Ausbildung Promos
 
                                 # Auswanderer / Emigrant -> in der eigenen Stadt
                                 if iUnitType == gc.getInfoTypeForString("UNIT_EMIGRANT"):
