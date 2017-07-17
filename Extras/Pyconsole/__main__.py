a05ab11aa8efd37d15884aeaac8d237cce618149
Olaf S <funwithkinect@googlemail.com>
2017-07-17 21:24:30 +0200

    Minor changes.

--- Extras/Pyconsole/__main__.py
+++ Extras/Pyconsole/__main__.py
@@ -4,5 +4,8 @@ import Civ4Shell
 port = Civ4Shell.PYCONSOLE_PORT
 if len(sys.argv) > 1:
     port = int(sys.argv[1])
-
-Civ4Shell.start(port=port)
+if len(sys.argv) > 2:
+    host = str(sys.argv[2])
+    Civ4Shell.start(port=port, host=host)
+else:
+    Civ4Shell.start(port=port)
