431ef8dbb09e6f3dc0697f17f3b5c3ddc8b47254
Ramkhamhaeng <none@none.de>
2017-07-17 18:56:37 +0200

    Allow host as second argument in Civ4Shell script

--- Extras/Pyconsole/__main__.py
+++ Extras/Pyconsole/__main__.py
@@ -5,4 +5,8 @@ port = Civ4Shell.PYCONSOLE_PORT
 if len(sys.argv) > 1:
     port = int(sys.argv[1])
 
-Civ4Shell.start(port=port)
+if len(sys.argv) > 2:
+    host = str(sys.argv[2])
+    Civ4Shell.start(port=port, host=host)
+else:
+    Civ4Shell.start(port=port)
